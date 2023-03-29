0567718a7df0760a37b557b7b0c257af271b7e62
Ashari4121 <ashari.ari71@gmail.com>
2023-03-29 23:19:00 +0800

    firsh commit

---
+++ resources/views/includes/script.blade.php
@@ -0,0 +1,8 @@
+<!-- Bootstrap core JavaScript -->
+<script src="/belajar/vendor/jquery/jquery.slim.min.js"></script>
+<script src="/belajar/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<script>
+    AOS.init();
+</script>
+<script src="/belajar/script/navbar-scroll.js"></script>
